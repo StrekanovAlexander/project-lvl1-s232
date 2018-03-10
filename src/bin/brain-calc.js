58bb6e90a202e4ff72b1413f9329b27bd82bb946
Strekanov Alexander <strekanov.alexander@gmail.com>
2018-03-10 18:26:30 +0200

    Update brain-calc.js

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 
-<<<<<<< HEAD
 import runGame from '..';
 import { taskHeader, task } from '../games/brain-calc';
 
 runGame(taskHeader, task);
-=======
-import { getCalc } from '..';
-
-getCalc();
->>>>>>> 4c2846e49afa7910cf098bb1730d70e93c16c416
