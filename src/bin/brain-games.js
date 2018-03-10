0d238d29c0f8db8f8272001e86285aa679630057
Strekanov Alexander <strekanov.alexander@gmail.com>
2018-03-10 17:32:08 +0200

    Add files via upload

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 
-import { getAnswer } from '..';
-
-console.log('Welcome to the Brain Games');
-let answer = getAnswer('\nMay I have your name? ');
-console.log(`Hi, ${answer}`);
+import { greetUser } from '..';
+greetUser();
--- src/bin/brain-gcd.js
+++ src/bin/brain-gcd.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+<<<<<<< HEAD
+import runGame from '..';
+import { taskHeader, task } from '../games/brain-gcd';
+
+runGame(taskHeader, task);
+=======
 import { getGcd } from '..';
 
 getGcd();
+>>>>>>> 4c2846e49afa7910cf098bb1730d70e93c16c416
